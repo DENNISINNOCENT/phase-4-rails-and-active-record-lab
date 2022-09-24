e16a7c1e216ddee1511a8e038e515e16f9b47782
DENNISINNOCENT <mutindahdennis@gmail.com>
2022-09-24 16:25:35 +0300

    commit tree 3a63aea0beadeaa41c75e94f67d8e144a5cd534a

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Student.create!(first_name:"Dennis",last_name:"Mutinda",grade:70)
